350bd45d469794672372b10ac194946f7c49ddfd
Cassidy Tillemans <cassidytillemans@Cassidys-MacBook-Pro.local>
2020-11-16 11:26:41 -0800

    builds initial haircut schema for testing

---
+++ backend/schemas/haircut.js
@@ -0,0 +1,46 @@
+import { string } from 'prop-types';
+
+export default {
+  title: 'Haircut',
+  name: 'haircut',
+  type: 'document',
+  fields: [
+    {
+      title: 'Hair cut title',
+      name: 'haircutTitle',
+      type: 'string',
+      validation: (Rule) => Rule.max(100),
+    },
+    {
+      title: 'Description',
+      name: 'description',
+      description: 'describe the haircut',
+      type: 'string',
+      validation: (Rule) => Rule.max(250),
+    },
+    {
+      title: 'Image 1',
+      name: 'image1',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+      fields: [
+        {
+          title: 'Image Description',
+          name: 'description',
+          type: 'string',
+          validation: (Rule) => Rule.max(50),
+        },
+      ],
+    },
+    {
+      title: 'Date',
+      name: 'date',
+      type: 'date',
+      options: {
+        calendarTodayLabel: true,
+      },
+    },
+  ],
+};
