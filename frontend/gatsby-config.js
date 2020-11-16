01e4112268e4938d054a803bbebc5a2e3f1ae719
Cassidy Tillemans <cassidytillemans@Cassidys-MacBook-Pro.local>
2020-11-16 12:49:26 -0800

    adds graphql API to connect gatsby to sanity cms

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -4,5 +4,20 @@ module.exports = {
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
-  plugins: [],
-}
+  plugins: [
+    {
+      resolve: `gatsby-source-sanity`,
+      options: {
+        projectId: `szqv597y`,
+        dataset: `production`,
+        // a token with read permissions is required
+        // if you have a private dataset
+        token: process.env.SANITY_TOKEN,
+
+        // If the Sanity GraphQL API was deployed using `--tag <name>`,
+        // use `graphqlTag` to specify the tag name. Defaults to `default`.
+        graphqlTag: 'default',
+      },
+    },
+  ],
+};
