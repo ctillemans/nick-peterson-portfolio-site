505d1684ca64f624a8738fa8b0ff6094a461a90c
Cassidy Tillemans <cassidytillemans@Cassidys-MacBook-Pro.local>
2020-11-19 12:47:23 -0800

    more styling for single cut component

--- frontend/src/components/CutsList.js
+++ frontend/src/components/CutsList.js
@@ -3,7 +3,12 @@ import styled from 'styled-components';
 import SingleCut from './SingleCut';
 
 const CutListStyles = styled.div`
-  display: flex;
+  /* width: 90vw; */
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  gap: 2rem;
+  margin: auto;
+  padding: 2rem;
 `;
 
 export default function CutsList({ cuts }) {
--- frontend/src/components/SingleCut.js
+++ frontend/src/components/SingleCut.js
@@ -3,18 +3,37 @@ import styled from 'styled-components';
 import Img from 'gatsby-image';
 
 const CutCardContainer = styled.div`
-  height: 300px;
-  min-width: 200px;
-  text-align: center;
+  position: relative;
+  min-height: 500px;
+  background: url(${(props) => props.image});
+  background-size: cover;
+  background-repeat: no-repeat;
   border-radius: 10px;
-  border: 1px solid black;
+  padding: 1rem;
+  transition: all 0.1s ease-in-out;
+  &:hover {
+    transform: scale(1.05);
+  }
+`;
+const CutContent = styled.div`
+  position: absolute;
+  top: 50%;
+  transform: translateX(-50%);
+  left: 50%;
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
 `;
 
 export default function SingleCut({ cut }) {
   return (
-    <CutCardContainer>
-      <h1>{cut.haircutTitle}</h1>
-      <Img fluid={cut.image1.asset.fluid} />
+    <CutCardContainer className='card' image={cut.image1.asset.fluid.src}>
+      <CutContent className='card__Content'>
+        <h1>{cut.haircutTitle}</h1>
+        <p>{cut.description}</p>
+      </CutContent>
     </CutCardContainer>
   );
 }
