0946c4c72644aab03875f1e37a8711b6f6bbc805
Cassidy Tillemans <cassidybtillemans@gmail.com>
2020-12-02 20:17:02 -0800

    fixes data schema for haircut to auto generate slugs and avoids duplicates with custom slugify function

--- backend/schemas/haircut.js
+++ backend/schemas/haircut.js
@@ -1,4 +1,24 @@
+import sanityClient from 'part:@sanity/base/client';
 import { string } from 'prop-types';
+var slugify = require('slugify');
+
+function asyncSlugify(input, type) {
+  console.log(input);
+  // make a slug from the input
+  const slug = slugify(input);
+  console.log(slug);
+
+  const query =
+    'count(*[_type=="haircut" && haircutSlug.current == $slug]{_id, haircutTitle})';
+  const params = { slug: slug };
+  return sanityClient.fetch(query, params).then((count) => {
+    console.log(`people with same slug: ${count}`);
+
+    return count >= 1 ? `${slug}-${count + 1}` : slug;
+  });
+  // search all slugs
+  //make it unique by adding the count to the end
+}
 
 export default {
   title: 'Haircut',
@@ -10,6 +30,15 @@ export default {
       name: 'haircutTitle',
       type: 'string',
       validation: (Rule) => Rule.max(100),
+    },
+    {
+      title: 'Slug',
+      name: 'haircutSlug',
+      type: 'slug',
+      options: {
+        source: 'haircutTitle',
+        slugify: asyncSlugify,
+      },
     },
     {
       title: 'Description',
