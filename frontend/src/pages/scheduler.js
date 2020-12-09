c665758bc1070ef5b51b3ef2a88595de3ce8c29a
Cassidy Tillemans <cassidybtillemans@gmail.com>
2020-12-08 22:36:37 -0800

    sets up rough outline for the single cuts page

--- frontend/src/pages/scheduler.js
+++ frontend/src/pages/scheduler.js
@@ -1,9 +1,10 @@
-import React from "react"
+import React from 'react';
+import TextCarousel from '../components/Carousel/TextCarousel';
 
 export default function scheduler() {
   return (
     <div>
       <h1>This is the Scheduler page</h1>
     </div>
-  )
+  );
 }
